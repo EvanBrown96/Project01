ebfa9243023b0a7ce0371ac7925feb89b14b84d8
Benjamin Streit <benjaminmstreit@gmail.com>
2018-10-02 14:23:12 -0500

    Getting menu.py up to spec.

--- menu.py
+++ menu.py
@@ -12,20 +12,36 @@ from executive import Executive
 #  @brief Prints menu and rules; Manages Executive instance
 class Menu:
 
+    """
+    Menu class to act as game menu
+
+    Attributes:
+        choice: Integer value to record whether or not user wants to play again
+
+        myGame: Instance of the Executive class for managing the game
+    """
+
     ## Constructor; initializes class variables
     #  @author: Ayah
     def __init__(self):
-        ## @var choice
-        #  flag for replay choice
+        """
+        Constructor for Menu class
+
+        Initializes all attributes
+        """
         self.choice = 0
-        ## @var myGame
-        #  instance of the executive class
         self.myGame = Executive()
 
     ## Handles any type error in users input
     #  @author: Ayah
     #  @returns user input when entered correctly
     def type_error_handler(self):
+        """
+        Handles any type error in users input
+
+        Returns:
+            check: Integer of users input if integer input
+        """
         while True:
             try:
                 check = int (input())
@@ -38,7 +54,9 @@ class Menu:
     ## Keep the game running until user chose to quit
     #  @authors: Ayah
     def game_menu(self):
-
+        """
+        Keeps the game running until user quits
+        """
         play_again = 1
         while not self.myGame.game_over:
             print("Please, chose from the menu:")
@@ -69,6 +87,9 @@ class Menu:
     ## Prints the game instructions
     #  @author: Ayah
     def game_rules(self):
+        """
+        Prints the game instructions
+        """
         print("""Welcome to Minesweepers!
 
 Here are the game instructions:
