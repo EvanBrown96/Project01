2605d5217ae6da352f24fff5d361a83238a21fc2
Benjamin Streit <benjaminmstreit@gmail.com>
2018-10-02 11:48:41 -0500

    Updating documentation for stopwatch.py.

--- stopwatch.py
+++ stopwatch.py
@@ -3,26 +3,48 @@ import time
 class Stopwatch:
 
     """
-    Simple stopwatch class
+    Simple stopwatch class with functionality for starting, stopping, and
+    getting current time on the stopwatch
+
+    Attributes:
+        start_time: Integer to record time (0 seconds) that stopwatch was started
+        end_time: Integer to record number of seconds since stopwatch started
     """
 
-    # Constructor
     def __init__(self):
-      # @var start_time : time stopwatch started
-      self.start_time = 0;
-      # @var end_time : time stopwatch ended
-      self.end_time = 0;
+        """
+        Constructor for Stopwatch
+        Sets both start_time and end_time member variables to 0
+        """
+        self.start_time = 0;
+        self.end_time = 0;
 
     # Starts stopwatch
     def start(self):
-      self.start_time = round(time.time())
+        """
+        Starts stopwatch by setting the start_time to time.time(),
+        rounded to the nearest whole number
+        """
+        self.start_time = round(time.time())
 
     # Starts stopwatch
     def stop(self):
+        """
+        Stops stopwatch by settingt the end_time equal to time.time() - start_time,
+        rounded to the nearest whole number
+        """
         self.end_time = round(time.time() - self.start_time)
 
     # Gets current time on stopwatch
     def currentTime(self):
+        """
+        Gets current time on stopwatch
+        If start_time is 0, returns 0
+        Else returns time.time() - start_time rounded to nearest whole number
+
+        Returns:
+            Current time on switch as an integer
+        """
         # account for stopwatch not being started yet
         # don't want to return current time UTC
         if self.start_time == 0:
